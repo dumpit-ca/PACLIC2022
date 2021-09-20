a7021b5f3f2ffefce1febd36c6405586a6c1e2ab
dumpitca28 <dumpitca@outlook.com>
2021-09-21 00:26:19 +0800

    added past conference cards

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -17,13 +17,169 @@
     </div>
 </div>
 {{-- Past conference --}}
-<section id="conference">
+<section class="mt-2" id="conference">
     <div class="container-fluid">
-        <h4>
+        {{-- Header --}}
+        <h4 class="text-center p-4">
             <span class="font-weight-bold" id="paclic-header">PACLIC</span>
             <span class="font-weight-bold" id="paclic-header-conference">CONFERENCES</span>
         </h4>
-
+        <p class="text-white text-center pb-5">Since the 18th Pacific Asia Conference on Language, Information and
+            Computation, each PACLIC conference is organized
+            under the auspices of the PACLIC Steering Committee.</p>
+        {{-- Past conference card --}}
+        <div class="row d-flex justify-content-center">
+            <div class="col-lg-3 col-sm-6 mx-3">
+                <div class="card p-2">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">PACLIC 34 (2020)</h5>
+                        <p class="card-text mb-1">
+                            <span>Dates:</span>
+                            <span>October 24-26, 2020</span>
+                        </p>
+                        <p class="card-text">
+                            <span>Venue:</span>
+                            <span>Vietnam National University, Hanoi, Vietnam (Online) </span>
+                        </p>               
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 mx-3">
+                <div class="card p-2">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">PACLIC 34 (2020)</h5>
+                        <p class="card-text mb-1">
+                            <span>Dates:</span>
+                            <span>October 24-26, 2020</span>
+                        </p>
+                        <p class="card-text">
+                            <span>Venue:</span>
+                            <span>Vietnam National University, Hanoi, Vietnam (Online) </span>
+                        </p>               
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 mx-3">
+                <div class="card p-2">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">PACLIC 34 (2020)</h5>
+                        <p class="card-text mb-1">
+                            <span>Dates:</span>
+                            <span>October 24-26, 2020</span>
+                        </p>
+                        <p class="card-text">
+                            <span>Venue:</span>
+                            <span>Vietnam National University, Hanoi, Vietnam (Online) </span>
+                        </p>               
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="p-4">
+            <hr>
+        </div>
+        {{-- Second row --}}
+        <div class="row d-flex justify-content-center">
+            <div class="col-lg-3 col-sm-6 mx-3">
+                <div class="card p-2">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">PACLIC 34 (2020)</h5>
+                        <p class="card-text mb-1">
+                            <span>Dates:</span>
+                            <span>October 24-26, 2020</span>
+                        </p>
+                        <p class="card-text">
+                            <span>Venue:</span>
+                            <span>Vietnam National University, Hanoi, Vietnam (Online) </span>
+                        </p>               
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 mx-3">
+                <div class="card p-2">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">PACLIC 34 (2020)</h5>
+                        <p class="card-text mb-1">
+                            <span>Dates:</span>
+                            <span>October 24-26, 2020</span>
+                        </p>
+                        <p class="card-text">
+                            <span>Venue:</span>
+                            <span>Vietnam National University, Hanoi, Vietnam (Online) </span>
+                        </p>               
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 mx-3">
+                <div class="card p-2">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">PACLIC 34 (2020)</h5>
+                        <p class="card-text mb-1">
+                            <span>Dates:</span>
+                            <span>October 24-26, 2020</span>
+                        </p>
+                        <p class="card-text">
+                            <span>Venue:</span>
+                            <span>Vietnam National University, Hanoi, Vietnam (Online) </span>
+                        </p>               
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="p-4">
+            <hr>
+        </div>
+        {{-- Third Row --}}
+        <div class="row d-flex justify-content-center">
+            <div class="col-lg-3 col-sm-6 mx-3">
+                <div class="card p-2">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">PACLIC 34 (2020)</h5>
+                        <p class="card-text mb-1">
+                            <span>Dates:</span>
+                            <span>October 24-26, 2020</span>
+                        </p>
+                        <p class="card-text">
+                            <span>Venue:</span>
+                            <span>Vietnam National University, Hanoi, Vietnam (Online) </span>
+                        </p>               
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 mx-3">
+                <div class="card p-2">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">PACLIC 34 (2020)</h5>
+                        <p class="card-text mb-1">
+                            <span>Dates:</span>
+                            <span>October 24-26, 2020</span>
+                        </p>
+                        <p class="card-text">
+                            <span>Venue:</span>
+                            <span>Vietnam National University, Hanoi, Vietnam (Online) </span>
+                        </p>               
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-sm-6 mx-3">
+                <div class="card p-2">
+                    <div class="card-body">
+                        <h5 class="card-title font-weight-bold">PACLIC 34 (2020)</h5>
+                        <p class="card-text mb-1">
+                            <span>Dates:</span>
+                            <span>October 24-26, 2020</span>
+                        </p>
+                        <p class="card-text">
+                            <span>Venue:</span>
+                            <span>Vietnam National University, Hanoi, Vietnam (Online) </span>
+                        </p>               
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="p-4">
+            <hr>
+        </div>
     </div>
 </section>
 
