809a31c79930e98546261c1875eb395aa4047892
dumpitca28 <dumpitca@outlook.com>
2021-09-21 03:46:06 +0800

    finalized about us page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -17,19 +17,19 @@
     </div>
 </div>
 {{-- Past conference --}}
-<section class="mt-2" id="conference">
+<section class="mt-2 bg-green">
     <div class="container-fluid">
-        {{-- Header --}}       
+{{-- Header --}}       
             <div class="floating-underline">
                 <span class="h4 text-center px-4 pt-4 pb-0 mb-0">
-                    <span class="font-weight-bold" id="paclic-header">PACLIC</span>
-                    <span class="font-weight-bold" id="paclic-header-conference">CONFERENCES</span>
+                    <span class="font-weight-bold paclic-header-white">PACLIC</span>
+                    <span class="font-weight-bold paclic-header-green">CONFERENCES</span>
                 </span> 
             </div>                      
         <p class="text-white text-center pb-5">Since the 18th Pacific Asia Conference on Language, Information and
             Computation, each PACLIC conference is organized
             under the auspices of the PACLIC Steering Committee.</p>
-        {{-- Past conference card --}}
+{{-- Past conference card --}}
         @for($i=0;$i<7;$i++)
         <div class="row d-flex justify-content-center">
             @for($j=0;$j<3;$j++)
@@ -58,11 +58,11 @@
 </section>
 <section class="background-page">
     <div class="container-fluid">
-        {{-- Organizers --}}
+{{-- Organizers --}}
         <div class="floating-underline alt">
             <span class="h4 text-center px-4 pt-4 pb-0 mb-0">
                 <span class="font-weight-bold" id="paclic-header-org">PACLIC</span>
-                <span class="font-weight-bold" id="paclic-header-organizers">ORGANIZERS</span>
+                <span class="font-weight-bold paclic-header-green">ORGANIZERS</span>
             </span> 
         </div>
     </div>
@@ -73,18 +73,56 @@
             </div>
             <div class="col-lg-6 pt-3">
                 <h3 class="font-weight-bold organizer-name">Lorem Ipsum</h3>
-                <p class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eligendi excepturi quidem repellendus tempore ad, obcaecati sint corrupti temporibus dolorem maxime nihil quasi est quam saepe neque libero nostrum tempora!</p>
+                <p class="text-left p-organizer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eligendi excepturi quidem repellendus tempore ad, obcaecati sint corrupti temporibus dolorem maxime nihil quasi est quam saepe neque libero nostrum tempora!</p>
             </div>
             <div class="col-lg-4 text-center mt-5">
                 <img class="img-fluid organizer" src="/img/businessmanV1.png" alt="org1">
             </div>
             <div class="col-lg-6 pt-sm-3">
                 <h3 class="font-weight-bold organizer-name mt-5 mt-sm">Lorem Ipsum</h3>
-                <p class="text-left">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eligendi excepturi quidem repellendus tempore ad, obcaecati sint corrupti temporibus dolorem maxime nihil quasi est quam saepe neque libero nostrum tempora!</p>
+                <p class="text-left p-organizer">Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia eligendi excepturi quidem repellendus tempore ad, obcaecati sint corrupti temporibus dolorem maxime nihil quasi est quam saepe neque libero nostrum tempora!</p>
             </div>
         </div> 
+    </div>   
+</section>
+{{-- Sponsors --}}
+<section class="bg-green">
+    <div class="floating-underline">
+        <span class="h4 text-center px-4 pt-4 pb-0 mb-0">
+            <span class="font-weight-bold paclic-header-white">PACLIC</span>
+            <span class="font-weight-bold paclic-header-green">SPONSORS</span>
+        </span> 
     </div>
-        
-    
+    <div class="containter">
+        <div class="row mx-auto justify-content-center py-5">
+            <div class="col-lg-2 col-sm-5">
+                <div class="card card-bg border-0">
+                    <img src="/img/businessmanV2.png" class="card-img-top my-3 px-2" alt="sponsors">
+                    <div class="card-body">
+                      <h5 class="card-title font-weight-bold sponsor-name text-white">Lorem Ipsum</h5>
+                      <p class="card-text text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste sint, totam ea provident perspiciatis labore?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-5">
+                <div class="card card-bg border-0">
+                    <img src="/img/businessmanV2.png" class="card-img-top my-3 px-2" alt="sponsors">
+                    <div class="card-body">
+                      <h5 class="card-title font-weight-bold sponsor-name text-white">Lorem Ipsum</h5>
+                      <p class="card-text text-white">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem voluptatum repellendus numquam voluptates tenetur saepe?</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-2 col-sm-5">
+                <div class="card card-bg border-0">
+                    <img src="/img/businessmanV2.png" class="card-img-top my-3 px-2" alt="sponsors">
+                    <div class="card-body">
+                      <h5 class="card-title font-weight-bold sponsor-name text-white">Lorem Ipsum</h5>
+                      <p class="card-text text-white">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Et harum esse ad veniam delectus quae.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 @endsection
