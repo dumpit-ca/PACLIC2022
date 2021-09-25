9418579170499aa9bd168bf2557cba7e461c7254
dumpitca28 <dumpitca@outlook.com>
2021-09-26 04:09:52 +0800

    justified all paragraphs

--- resources/views/speakers.blade.php
+++ resources/views/speakers.blade.php
@@ -16,6 +16,9 @@
     .speaker-info {
         font-size: 15px!important;
     }
+    .brief-info {
+        text-align: justify;
+    }
 </style>
 <div class="floating-underline alt mt-5">
     <span class="h4 text-center px-4 pt-4 pb-0 mb-0">
@@ -36,7 +39,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-7 mx-5">
+        <div class="col-lg-7 mx-5 brief-info">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati doloribus earum nulla corrupti dolor odio voluptatem expedita deleniti quasi, provident illum tenetur autem perspiciatis esse veniam recusandae? Nostrum voluptas nihil, doloremque tenetur ipsam esse magnam vero, saepe consequuntur officia aliquid enim perferendis repellendus, beatae labore architecto aspernatur! Voluptatibus, odio architecto.
         </div>
     </div>
@@ -52,10 +55,10 @@
                 </div>
             </div>
         </div>
-        <div class="col-lg-7 mx-5">
+        <div class="col-lg-7 mx-5 brief-info">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati doloribus earum nulla corrupti dolor odio voluptatem expedita deleniti quasi, provident illum tenetur autem perspiciatis esse veniam recusandae? Nostrum voluptas nihil, doloremque tenetur ipsam esse magnam vero, saepe consequuntur officia aliquid enim perferendis repellendus, beatae labore architecto aspernatur! Voluptatibus, odio architecto.
         </div>
     </div>
-    <hr class="speaker-hr">
+    <hr class="speaker-hr my-5">
 </div>
 @endsection
