5fb0de1dd255eb5fb18eaa5e103570d176c814a0
dumpitca28 <dumpitca@outlook.com>
2021-09-30 18:04:53 +0800

    finished the timeline and program

--- resources/views/program.blade.php
+++ resources/views/program.blade.php
@@ -7,12 +7,12 @@
 <div class="container-fluid">
   <div class="row justify-content-center">
     <div class="col-lg-2 py-5 mr-4">
-      <select class="form-control text-white font-weight-bold select-dates">
-        <option class="font-weight-bold">October 20, 2022</option>
-        <option class="font-weight-bold">October 21, 2022</option>
-        <option class="font-weight-bold">October 22, 2022</option>
+      <select class="form-control text-white font-weight-bold select-dates" id="dates">
+        <option value="20" class="font-weight-bold" selected>October 20, 2022</option>
+        <option value="21" class="font-weight-bold">October 21, 2022</option>
+        <option value="22" class="font-weight-bold">October 22, 2022</option>
       </select>
-    </div>    
+    </div>
     <div class="col-lg-2 py-5">
       <button class="btn-green font-weight-bold border-0 p-2">
         Manila Time - 09:00 AM (9:00)
@@ -25,4 +25,228 @@
     </div>
   </div>
 </div>
+<div class="container justify-content-center my-5">
+  {{-- October 20, 2022 --}}
+  <div class="contents active" id="20-contents">
+    <section class="bg-green" id="program-radius">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5 justify-content-center">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">October 20, 2022</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+    </section>
+  </div>
+  {{-- October 21, 2022 --}}
+  <div class="contents" id="21-contents">
+    <section class="bg-green" id="program-radius">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">October 21, 2022</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+    </section>
+  </div>
+  {{-- October 22, 2022 --}}
+  <div class="contents" id="22-contents">
+    <section class="bg-green" id="program-radius">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">October 22, 2022</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+      <hr class="program-hr">
+      <div class="row p-4 mx-auto align-items-center">
+        <div class="col-lg-7">
+          <label class="font-weight-bold text-white px-5">09:00 - 09:10</label>
+        </div>
+        <div class="col-lg-4 justify-content-end ml-auto">
+          <p class="text-white">Program Name</p>
+          <p class="text-white">Name of speaker</p>
+          <p class="text-white">Title of speaker</p>
+        </div>
+      </div>
+    </section>
+  </div>
+</div>
+@endsection
+@section('script')
+<script type="text/javascript">
+  $(document).ready(() => {
+    $('#dates').on('change', (e) => {
+      let obj = $(e.currentTarget);
+      console.log(obj)
+      $('.contents').removeClass('active');
+      $('#' + obj.val() + '-contents').addClass('active');
+    });
+  });
+</script>
 @endsection
--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -7,7 +7,7 @@
     <title>{{env("APP_NAME")}}</title>
     <link rel="icon" type="image/png" href="/img/sample_logo.png">
 
-    {{-- Bootstrap --}}
+    {{-- Bootstrap 4.6 --}}
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
         integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
@@ -18,6 +18,10 @@
     <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="/css/master.css">
     <link rel="stylesheet" href="/css/@yield('css').css">
+    {{-- JQuery --}}
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+    integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
 </head>
 
 <body>
@@ -144,15 +148,18 @@
             </div>
         </div>
     </section>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+    {{-- Script --}}
+		<!-- Removes the code that shows up when script is disabled/not allowed/blocked -->
+		<script type="text/javascript" id="for-js-disabled-js">$('head').append('<style id="for-js-disabled">#js-disabled { display: none; }</style>');$(document).ready(function() {$('#js-disabled').remove();$('#for-js-disabled').remove();$('#for-js-disabled-js').remove();});</script>
+        {{-- Popper.js 1.16.1 --}}
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
         integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
+        </script>
+        {{-- Bootstrap 4.6 --}}
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.min.js"
         integrity="sha384-+YQ4JLhjyBLPDQt//I+STsc9iw4uQqACwlvpslubQzn4u2UU2UFM80nGisd026JF" crossorigin="anonymous">
-    </script>
+        </script>
+        @yield('script')
 </body>
 
 </html>
